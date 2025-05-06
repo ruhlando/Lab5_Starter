4f682a6e90879b17cf9dbe7815fe59601e231d14
ruhlando <fabio.ruhland@gmail.com>
2025-05-05 21:19:02 -0700

    add available voices dropdown

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,51 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let synth = window.speechSynthesis;
+
+  const inputTxt = document.getElementById("text-to-speak");
+  const voiceSelect = document.getElementById("voice-select");
+
+  let voices = [];
+
+
+  function populateVoiceList() {
+    voices = synth.getVoices().sort(function (a, b) {
+      const aname = a.name.toUpperCase();
+      const bname = b.name.toUpperCase();
+  
+      if (aname < bname) {
+        return -1;
+      } else if (aname == bname) {
+        return 0;
+      } else {
+        return +1;
+      }
+    });
+    const selectedIndex =
+      voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+    voiceSelect.innerHTML = "";
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += " -- DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+    voiceSelect.selectedIndex = selectedIndex;
+  }
+  
+  populateVoiceList();
+
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+  
+  
 }
