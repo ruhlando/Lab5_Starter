99231e063cda874adca2a2749e376f1056658d98
ruhlando <fabio.ruhland@gmail.com>
2025-05-05 17:26:38 -0700

    add playing sound with button

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -7,9 +7,12 @@ function init() {
   let volumeControls = document.getElementById("volume");
 
   let hornImage = document.querySelector("img");
-  let hornSound = document.getElementsByClassName("hidden");
+/*   let hornSound = document.getElementsByClassName("hidden"); */
 
   let volumeImage = document.querySelector("#volume-controls img:first-of-type");
+
+  let playHorn = document.querySelector("button");
+  let hornSound = new Audio();
 
 
   hornSelect.addEventListener("change", function() {
@@ -18,9 +21,11 @@ function init() {
     if (selectedHorn == "air-horn") {
       hornImage.src = "assets/images/air-horn.svg";
       hornSound.src = "assets/audio/air-horn.mp3";
+
     } else if (selectedHorn == "car-horn") {
         hornImage.src = "assets/images/car-horn.svg";
         hornSound.src = "assets/audio/car-horn.mp3";
+        
     } else if (selectedHorn == "party-horn") {
         hornImage.src = "assets/images/party-horn.svg";
         hornSound.src = "assets/audio/party-horn.mp3";
@@ -29,27 +34,28 @@ function init() {
 
   volumeControls.addEventListener("input", function() {
     let selectedVolume = volumeControls.value;
-    console.log(selectedVolume);
-    let hornVolume = document.createElement("audio");
-    hornVolume.volume = selectedVolume / 100;
+    /* let hornVolume = document.createElement("audio");
+    hornVolume.volume = selectedVolume / 100; */
+    hornSound.volume = selectedVolume / 100;
 
 
     if (selectedVolume == 0) {
       volumeImage.src = "assets/icons/volume-level-0.svg";
-      console.log(hornVolume.volume);
     }
     else if (selectedVolume < 33) {
       volumeImage.src = "assets/icons/volume-level-1.svg";
-      console.log(hornVolume.volume);
     }
     else if (selectedVolume < 67) {
       volumeImage.src = "assets/icons/volume-level-2.svg";
-      console.log(hornVolume.volume);
     }
     else {
       volumeImage.src = "assets/icons/volume-level-3.svg";
-      console.log(hornVolume.volume);
     }
+
+  });
+
+  playHorn.addEventListener("click", function(){
+    hornSound.play();
 
   });
   
