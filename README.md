42c3ff8414e0bb8fcc30c0f29ff3779c9fc3aae0
ruhlando <fabio.ruhland@gmail.com>
2025-05-06 20:13:10 -0700

    add unit tests

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Lab 5 - Starter
 - My Name: Fabio Ruhland
-- Lab Partner: XY tbd
+
+### Pt 3. Unit Testing with Jest
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user. </br>
+   
+   No, because sending a message involves multiple parts working together (input, network, receiver), and unit tests only check small, isolated parts.
+
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters. </br>
+   
+   Yes, because it’s a simple, isolated rule that can be tested on its own without involving the whole app.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,97 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+// isPhoneNumber tests: 
+
+test('valid phone number: (123) 456-7890', () => {
+  expect(isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+
+test('valid phone number: 123-456-7890', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('invalid phone number: 1234567890', () => {
+  expect(isPhoneNumber('1234567890')).toBe(false);
+});
+
+test('invalid phone number: abc-def-ghij', () => {
+  expect(isPhoneNumber('abc-def-ghij')).toBe(false);
+});
+
+
+// isEmail tests:
+
+test('valid email: test@example.com', () => {
+  expect(isEmail('test@example.com')).toBe(true);
+});
+
+test('valid email: user123@domain.org', () => {
+  expect(isEmail('user123@domain.org')).toBe(true);
+});
+
+test('invalid email: test@.com', () => {
+  expect(isEmail('test@.com')).toBe(false);
+});
+
+test('invalid email: @domain.com', () => {
+  expect(isEmail('@domain.com')).toBe(false);
+});
+
+
+// isStrongPassword tests
+
+test('valid password: Abcd1234', () => {
+  expect(isStrongPassword('Abcd1234')).toBe(true);
+});
+
+test('valid password: a_b1', () => {
+  expect(isStrongPassword('a_b1')).toBe(true);
+});
+
+test('invalid password: 123abc', () => {
+  expect(isStrongPassword('123abc')).toBe(false); // starts with number
+});
+
+test('invalid password: a', () => {
+  expect(isStrongPassword('a')).toBe(false); // too short
+});
+
+
+// isDate tests
+
+test('valid date: 12/25/2023', () => {
+  expect(isDate('12/25/2023')).toBe(true);
+});
+
+test('valid date: 1/1/1999', () => {
+  expect(isDate('1/1/1999')).toBe(true);
+});
+
+test('invalid date: 12/25/23', () => {
+  expect(isDate('12/25/23')).toBe(false); // year must be 4 digits
+});
+
+test('invalid date: 2023/12/25', () => {
+  expect(isDate('2023/12/25')).toBe(false); // wrong format
+});
+
+
+// isHexColor tests
+
+test('valid hex color: #fff', () => {
+  expect(isHexColor('#fff')).toBe(true);
+});
+
+test('valid hex color: 123ABC', () => {
+  expect(isHexColor('123ABC')).toBe(true);
+});
+
+test('invalid hex color: #ggg', () => {
+  expect(isHexColor('#ggg')).toBe(false); // invalid characters
+});
+
+test('invalid hex color: #12345', () => {
+  expect(isHexColor('#12345')).toBe(false); // invalid length
+});
